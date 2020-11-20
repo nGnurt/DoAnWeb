242b41a60cdd5254f1829e8809c01465b9e22a7e
nguyenkhanh2003 <74747143+nguyenkhanh2003@users.noreply.github.com>
2020-11-20 20:45:43 +0700

    dddd

---
+++ DoAn_ASPNETCORE/Areas/User/Data/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn_ASPNETCORE.Areas.User.Data
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
